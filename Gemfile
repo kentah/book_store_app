393443e316ab14712c4ef229ab54323272ec6c22
kentah <kenthow@gmail.com>
2016-04-11 20:49:13 -0500

    implement validating author

--- Gemfile
+++ Gemfile
@@ -36,18 +36,21 @@ group :development, :test do
   gem 'rspec-rails', '3.2.3'
   gem 'spring-commands-rspec'
   gem 'guard-rspec', require: false
+  gem 'fabrication'
 end
 
 group :test do
-
+  
   gem 'capybara', '2.4.4'
+  gem 'faker'
 
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'pry'
+  gem 'pry-nav'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
---
+++ spec/models/author_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+
+RSpec.describe Author, :type => :model do
+
+  it 'requires a first name' do
+    author = Fabricate.build(:author, first_name: nil)
+    
+    expect(author).not_to be_valid
+    expect(author.errors[:first_name].any?).to be_truthy
+  end
+  
+  it 'requires a last name' do
+    author = Fabricate.build(:author, last_name: nil)
+
+    expect(author).not_to be_valid
+    expect(author.errors[:last_name].any?).to be_truthy
+  end
+
+    
+
+  describe "#full_name" do
+    it 'returns the full name of the author' do
+      author = Fabricate(:author, first_name: 'Kent', last_name: 'Howard')
+
+      expect(author.full_name).to eq('Kent Howard')
+    end
+  end
+  
+end
