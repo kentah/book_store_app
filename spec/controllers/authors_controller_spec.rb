cc85838f8e18aa9ea82b8956848844d257f67af1
kentah <kenthow@gmail.com>
2016-04-30 18:14:56 -0500

    Implement Author create successful path

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -27,4 +27,32 @@ RSpec.describe AuthorsController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'POST #create' do
+    context 'a successful create' do
+      it 'saves the new author object' do
+        post :create, author: Fabricate.attributes_for(:author)
+
+        expect(Author.count).to eq(1)
+      end
+
+      it 'redirects to the author show action' do
+        post :create, author: Fabricate.attributes_for(:author)
+
+        expect(response).to redirect_to author_path(Author.last)
+      end
+
+      it 'set the success flash message' do
+        post :create, author: Fabricate.attributes_for(:author)
+
+        expect(flash[:success]).to eq('Author has been created')
+      end
+    end
+
+    context 'an unsuccessful create' do
+      
+    end
+    
+  end
+  
 end
